a46b9866afa6fa0a158e58d55aaf1fc99ea5eb12
Ubuntu <ubuntu@ip-172-31-9-187.us-east-2.compute.internal>
2020-04-18 04:37:26 +0000

    misc

--- kubernetes-manifests/run_application.sh
+++ kubernetes-manifests/run_application.sh
@@ -1,11 +1,26 @@
+echo -----------------------------------------
+
+sudo kubectl delete deployment account-depl
+sudo kubectl delete service service-account
+
+sudo kubectl delete deployment customer-depl
+sudo kubectl delete service service-customer
+
+sudo kubectl delete deployment derby-db-depl
+sudo kubectl delete service derby-db-service
+
+echo -----------------------------------------
+
 sudo kubectl create -f  derby-db-depl.yaml
 sudo kubectl create -f  derby-db-service.yaml
-echo sleeping for 60 seconds
-sleep 60
+echo -----------------------------------------
+#echo sleeping for 60 seconds
+#sleep 60
 sudo kubectl create -f  account-depl.yaml
 sudo kubectl create -f  account-service.yaml
-echo sleeping for 60 seconds
-sleep 60
+echo -----------------------------------------
+#echo sleeping for 60 seconds
+#sleep 60
 sudo kubectl create -f  customer-depl.yaml
 sudo kubectl create -f  customer-service.yaml
-
+echo -----------------------------------------
