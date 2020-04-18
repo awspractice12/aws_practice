23dad4e868467d35e3596bf2ed9d3488c2e15fb7
Shibu TN <tnshibu@yahoo.com>
2020-04-18 21:05:04 +0530

    ss-cloud-poc

--- java-micro-service-projects/account-service/docker_image_create.sh
+++ java-micro-service-projects/account-service/docker_image_create.sh
@@ -1,7 +1,7 @@
 sudo docker build . -t account-service:1.0
 
-sudo docker tag        account-service:1.0 local:account-service-1.0
-sudo docker tag        account-service:1.0 localhost:5000/local:account-service-1.0
-sudo docker tag        account-service:1.0 732631625641.dkr.ecr.us-east-2.amazonaws.com/ss-cloud-poc-container-repo:account-service-1.0
+sudo docker tag        account-service:1.0 ss-cloud-poc:account-service-1.0
+sudo docker tag        account-service:1.0 localhost:5000/ss-cloud-poc:account-service-1.0
+sudo docker tag        account-service:1.0 732631625641.dkr.ecr.us-east-2.amazonaws.com/ss-cloud-poc:account-service-1.0
 
-sudo docker push       localhost:5000/local:account-service-1.0
+sudo docker push       localhost:5000/ss-cloud-poc:account-service-1.0
