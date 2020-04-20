2688765cfdcefe0f128857f06aaae51138198554
Ubuntu <ubuntu@ip-172-31-41-2.us-east-2.compute.internal>
2020-04-20 04:14:17 +0000

    loan

--- kubernetes-manifests/run_application.sh
+++ kubernetes-manifests/run_application.sh
@@ -6,6 +6,9 @@ sudo kubectl delete service service-account
 sudo kubectl delete deployment customer-depl
 sudo kubectl delete service service-customer
 
+sudo kubectl delete deployment loan-depl
+sudo kubectl delete service service-loan
+
 sudo kubectl delete deployment derby-db-depl
 sudo kubectl delete service derby-db-service
 
@@ -24,3 +27,9 @@ echo -----------------------------------------
 sudo kubectl create -f  customer-depl.yaml
 sudo kubectl create -f  customer-service.yaml
 echo -----------------------------------------
+#echo sleeping for 60 seconds
+#sleep 60
+sudo kubectl create -f  loan-depl.yaml
+sudo kubectl create -f  loan-service.yaml
+echo -----------------------------------------
+
