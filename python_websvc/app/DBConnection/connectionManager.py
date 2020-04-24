814e5648a52ddaaed8ecc85f7604520289660c95
vagrant <vagrant@pocappserv>
2020-04-24 20:13:24 +0000

    update

--- python_websvc/app/DBConnection/connectionManager.py
+++ python_websvc/app/DBConnection/connectionManager.py
@@ -3,5 +3,5 @@ import os
 from os import environ
 
 class connectionManager:
-     ddbres = boto3.resource('dynamodb', region_name=environ.get('REGION'), endpoint_url=environ.get('END_URL'))
-     ddbcln = boto3.client('dynamodb', region_name=environ.get('REGION'), endpoint_url=environ.get('END_URL'))
+     ddbres = boto3.resource('dynamodb')
+     ddbcln = boto3.client('dynamodb')
