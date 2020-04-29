05b6075e9f9ea038900bd1f1f70809f457800b3c
Shibu TN <tnshibu@yahoo.com>
2020-04-30 01:02:27 +0530

    misc

--- aws-client-commands/lambda/sspoc_main.py
+++ aws-client-commands/lambda/sspoc_main.py
@@ -1,4 +1,12 @@
-# Simple as it gets
-def add(event, context):
-    print 'return from ss-poc-lambda'
-    return 'return from ss-poc-lambda'
+import boto3
+pipeline = boto3.client('codepipeline')
+
+def lambda_handler(event, context):
+    print ("printing from ss-poc-lambda-2")
+
+    # stuff
+
+    response = pipeline.put_job_success_result(
+        jobId=event['CodePipeline.job']['id']
+    )
+    return response
