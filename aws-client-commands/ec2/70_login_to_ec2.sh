917740e2bc63998c9c34fd733cddfbb675052d4a
Shibu TN <tnshibu@yahoo.com>
2020-05-05 04:09:30 +0530

    more2

--- aws-client-commands/ec2/70_login_to_ec2.sh
+++ aws-client-commands/ec2/70_login_to_ec2.sh
@@ -1,2 +1,2 @@
-ssh  -o "StrictHostKeyChecking no" -i ~/MyKeyPair2.pem ubuntu@`cat ec2-hosts`
-#ssh   -i ~/MyKeyPair2.pem ubuntu@`cat ec2-hosts`
+ssh  -o "StrictHostKeyChecking no" -i ~/MyKeyPair5.pem ubuntu@`cat ec2-hosts`
+#ssh   -i ~/MyKeyPair5.pem ubuntu@`cat ec2-hosts`
