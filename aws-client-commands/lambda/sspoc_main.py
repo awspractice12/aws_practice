bc733d37f72143f8569ae57ad1dd8f98528f6dfe
Shibu TN <tnshibu@yahoo.com>
2020-05-05 04:03:46 +0530

    more

--- aws-client-commands/lambda/sspoc_main.py
+++ aws-client-commands/lambda/sspoc_main.py
@@ -1,6 +1,8 @@
 import boto3
 import subprocess
 import sys
+import json
+
 
 pipeline = boto3.client('codepipeline')
 s3_bucket = boto3.resource('s3')
@@ -9,32 +11,39 @@ def exec_cmd(command):
     p=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         print(line)
-    
+
 def add(event, context):
     print ("printing from ss-poc-lambda-2")
 
-    # command='mkdir -v /tmp/code/'
-    # exec_cmd(command)
+    exec_cmd('mkdir -v /tmp/code/')
 
+    response = s3_bucket.Object("ss-poc-shibu", "derby-db-depl.yaml").download_file(f'/tmp/code/derby-db-depl.yaml')
+    response = s3_bucket.Object("ss-poc-shibu", "derby-db-service.yaml").download_file(f'/tmp/code/derby-db-service.yaml')
 
-    # response = s3_bucket.Object("ss-poc-code", "account-depl.yaml").download_file(f'/tmp/code/account-depl.yaml')
-    # response = s3_bucket.Object("ss-poc-code", "account-service.yaml").download_file(f'/tmp/code/account-service.yaml')
+    response = s3_bucket.Object("ss-poc-shibu", "account-depl.yaml").download_file(f'/tmp/code/account-depl.yaml')
+    response = s3_bucket.Object("ss-poc-shibu", "account-service.yaml").download_file(f'/tmp/code/account-service.yaml')
 
-    # command='ls -l /tmp/code/'
-    # exec_cmd(command)
-    
-    # command='kubectl --version'
-    # exec_cmd(command)
+    response = s3_bucket.Object("ss-poc-shibu", "customer-depl.yaml").download_file(f'/tmp/code/customer-depl.yaml')
+    response = s3_bucket.Object("ss-poc-shibu", "customer-service.yaml").download_file(f'/tmp/code/customer-service.yaml')
 
-    command='/opt/kubectl/kubectl --version'
-    exec_cmd(command)
-    # command='/opt/awscli/bin/aws s3 cp s3://ss-poc-code/code.zip /tmp/code/'
-    # exec_cmd(command)
+    response = s3_bucket.Object("ss-poc-shibu", "loan-depl.yaml").download_file(f'/tmp/code/loan-depl.yaml')
+    response = s3_bucket.Object("ss-poc-shibu", "loan-service.yaml").download_file(f'/tmp/code/loan-service.yaml')
 
+    exec_cmd("/opt/aws eks update-kubeconfig --name ss-poc-cluster  --kubeconfig /tmp/code/kubeconfig --role-arn arn:aws:iam::560773393352:role/sspoc_basic_lambda_role") 
 
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/derby-db-depl.yaml")
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/derby-db-service.yaml")
 
-    print ("Completed Unzippping.....")
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/account-depl.yaml")
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/account-service.yaml")
 
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/customer-depl.yaml")
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/customer-service.yaml")
+
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/loan-depl.yaml")
+    exec_cmd("/opt/kubectl/kubectl --kubeconfig /tmp/code/kubeconfig apply -f /tmp/code/loan-service.yaml")
+
+    print ("Completed .....")
     
     response = pipeline.put_job_success_result(
         jobId=event['CodePipeline.job']['id']
