93911327e26a10c8b3ebbfc43a51abc286aa4940
shibu@vagrant.com <shibu@vagrant.com>
2020-06-02 04:21:52 +0000

    keypair-10

--- aws-client-commands/ec2/40_create_ec2_instance_t2_micro.sh
+++ aws-client-commands/ec2/40_create_ec2_instance_t2_micro.sh
@@ -1,6 +1,6 @@
 # Create instance
-aws ec2 --profile shibutrial run-instances --image-id ami-0fc20dd1da406780b --count 1 --instance-type t2.micro --key-name MyKeyPair5 --security-groups shibu-sg --user-data file://software_install_on_startup.txt  1>/dev/null
+aws ec2 --profile shibutrial run-instances --image-id ami-0fc20dd1da406780b --count 1 --instance-type t2.micro --key-name MyKeyPair10 --security-groups shibu-sg --user-data file://software_install_on_startup.txt  1>/dev/null
 sleep 60
 ./50_get_public_ipaddress.sh
-scp -i  ~/MyKeyPair5.pem bash_aliases.txt ubuntu@`cat ec2-hosts`:.bash_aliases
+scp -i  ~/MyKeyPair10.pem bash_aliases.txt ubuntu@`cat ec2-hosts`:.bash_aliases
 
--- aws-client-commands/ec2/70_login_to_ec2.sh
+++ aws-client-commands/ec2/70_login_to_ec2.sh
@@ -1,2 +1,2 @@
-ssh  -o "StrictHostKeyChecking no" -i ~/MyKeyPair5.pem ubuntu@`cat ec2-hosts`
-#ssh   -i ~/MyKeyPair5.pem ubuntu@`cat ec2-hosts`
+ssh  -o "StrictHostKeyChecking no" -i ~/MyKeyPair10.pem ubuntu@`cat ec2-hosts`
+#ssh   -i ~/MyKeyPair10.pem ubuntu@`cat ec2-hosts`
